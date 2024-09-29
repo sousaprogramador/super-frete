6bc42b934715bc996011bf4826044094cb9818d9
Mateus Sousa <sousa.programador@gmail.com>
2024-09-29 19:49:24 -0300

    refactor code

--- functions/src/index.js
+++ functions/src/index.js
@@ -5,14 +5,11 @@ initializeApp();
 
 const recordController = require('./controllers/recordController');
 const { onDocumentCreated } = require('firebase-functions/v2/firestore');
-const recordTrigger = require('./triggers/recordTrigger');
+const { onNewRecordCreated } = require('./triggers/recordTrigger');
 
 exports.createRecord = functions.onRequest(recordController.createRecord);
 exports.getRecord = functions.onRequest(recordController.getRecord);
 exports.updateRecord = functions.onRequest(recordController.updateRecord);
 exports.deleteRecord = functions.onRequest(recordController.deleteRecord);
 
-exports.onNewRecordCreated = onDocumentCreated(
-  'test_collection/{docId}',
-  recordTrigger.onNewRecordCreated
-);
+exports.onNewRecordCreated = onNewRecordCreated;
--- functions/src/triggers/recordTrigger.js
+++ functions/src/triggers/recordTrigger.js
@@ -5,20 +5,20 @@ exports.onNewRecordCreated = onDocumentCreated(
   'test_collection/{docId}',
   async (event) => {
     const snapshot = event.data;
-    if (!snapshot) return null;
-
+    if (!snapshot) {
+      console.log('No data associated with the event');
+      return null;
+    }
     const newDocRef = snapshot.ref;
     const currentIdSnapshot = await getFirestore()
       .collection('test_collection')
       .orderBy('increment_id', 'desc')
       .limit(1)
       .get();
-
     let currentId = 1;
     if (!currentIdSnapshot.empty) {
       currentId = currentIdSnapshot.docs[0].data().increment_id + 1;
     }
-
     return newDocRef.update({ increment_id: currentId });
   }
 );
