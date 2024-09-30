e2181cb409d4959bbb0b294f9b553eca598c8ef1
Mateus Sousa <sousa.programador@gmail.com>
2024-09-29 22:10:17 -0300

    clear code

--- functions/src/__tests__/firebaseTestSetup.js
+++
@@ -1,8 +0,0 @@
-const admin = require('firebase-admin');
-
-// Inicializar o Firebase Admin SDK
-admin.initializeApp({
-  projectId: 'super-frete-ea193', // Substitua pelo seu ID do projeto Firebase
-});
-
-module.exports = admin;
